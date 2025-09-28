17ba3e74d2f98aa9b377a04f9a66d476db80fc89
Sylvester Alderson <khaositsylvester@gmail.com>
2025-09-28 13:59:56 +0300

    start

---
+++ src/main/java/com/dragofox/fennecfox/world/entity/FennecEntity.java
@@ -0,0 +1,39 @@
+package com.dragofox.fennecfox.world.entity;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.animatable.manager.AnimatableManager;
+
+public class FennecEntity extends Animal implements GeoEntity {
+
+    protected FennecEntity(EntityType<? extends Animal> type, Level level) {
+        super(type, level);
+    }
+
+    @Override
+    public boolean isFood(ItemStack itemStack) {
+        return false;
+    }
+
+    @Override
+    public @Nullable AgeableMob getBreedOffspring(ServerLevel serverLevel, AgeableMob ageableMob) {
+        return null;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return null;
+    }
+}
---
+++ src/main/java/com/dragofox/fennecfox/world/entity/MobEntites.java
@@ -0,0 +1,31 @@
+package com.dragofox.fennecfox.world.entity;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
+
+import static com.dragofox.fennecfox.fennecfox.MODID;
+
+
+public class MobEntites {
+    public static final DeferredRegister<EntityType<?>> ENTITIES =
+            DeferredRegister.createEntities(MODID);
+
+    public static final Supplier<EntityType<FennecEntity>> FENNEC_ENTITY = ENTITIES
+            .register("fennec_fox",
+                    () -> EntityType.Builder.of(
+                            FennecEntity::new,
+                            MobCategory.CREATURE
+                        )
+                            .sized(0.9f, 0.7f)
+                            .build(ResourceKey.create(
+                                    Registries.ENTITY_TYPE,
+                                    ResourceLocation.fromNamespaceAndPath("fennecmod", "fennec")
+                            ))
+                );
+}
